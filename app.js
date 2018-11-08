875dcbfeba241bc310714f7a1fd5e8f775dc2557
elharony <p.yahyaelharony@gmail.com>
2018-11-08 22:17:47 +0200

    feat: Change the 'border-radius' based on all 4 inputs

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // Cache the Border Corners' Inputs
-const c1 = document.querySelector("#corner-1")
+const corners = document.querySelectorAll(".border-corner"),
+        c1 = document.querySelector("#corner-1"),
+        c2 = document.querySelector("#corner-2"),
+        c3 = document.querySelector("#corner-3"),
+        c4 = document.querySelector("#corner-4")
 
 // Cache the demo container
 const demo = document.querySelector(".demo")
 
 
-// Attach an Event to it
-c1.addEventListener("change", () => {
-    demo.style.borderRadius = c1.value + 'px'
-    console.log(demo.style)
-    console.log("Border Radius: ", demo.style.borderRadius)
-    console.log("Changed Value: ", c1.value)
-})
+for(let i = 0; i < corners.length; i++) {
+    // Attach an Event to each 'Corner's Input' change
+    corners[i].addEventListener("change", () => {
+        // Change the 'border-radius' value of the demo
+        let value = `${c1.value}px ${c2.value}px ${c3.value}px ${c4.value}px`
+        demo.style.borderRadius = value
+    })
+}
