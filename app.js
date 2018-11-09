b5c4a00991e857d1025655a01b31b7dded89716f
elharony <p.yahyaelharony@gmail.com>
2018-11-09 02:40:42 +0200

    refactor: Wrap separated code into a function

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ const code = document.querySelector(".code")
 for(let i = 0; i < corners.length; i++) {
     // Attach an Event to each 'Corner's Input' change
     corners[i].addEventListener("change", () => {
-        // Change the 'border-radius' value of the demo
-        let value = `${c1.value}px ${c2.value}px ${c3.value}px ${c4.value}px`
-        demo.style.borderRadius = value
-        // View the code
-        code.innerHTML = showCode(c1.value, c2.value, c3.value, c4.value)
+        applyDemo()
     })
 }
 
@@ -28,6 +24,14 @@ function showCode(c1, c2, c3, c4) {
 <span>border-radius: ${c1}px ${c2}px ${c3}px ${c4}px;</span>
     `
     return codeNoPrefix
+}
+
+function applyDemo() {
+    // Change the 'border-radius' value of the demo
+    let value = `${c1.value}px ${c2.value}px ${c3.value}px ${c4.value}px`
+    demo.style.borderRadius = value
+    // View the code
+    code.innerHTML = showCode(c1.value, c2.value, c3.value, c4.value)
 }
 
 // Copy To Clipboard
